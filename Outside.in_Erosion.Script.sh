399899da6436d2980369655bff62f58685013531
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-03-31 21:59:20 -0400

    Outside.in_Erosion.Script.sh: Refactored with new erosion (WIP)

--- Outside.in_Erosion.Script.sh
+++ Outside.in_Erosion.Script.sh
@@ -1,31 +1,50 @@
 #!/bin/bash
 # This script is eroding the T2seg manual mask from the outside inwards.
-# Tested with SCT cd64e67844e7e4db62665d0e076a1dd816298ac2
+# Tested with SCT e5eee1f18f4cc3655dc42c565ffac5b2453af2d1
+
+# PARAMETERS
+# Set the number of erosion steps. On the PAM50, there is nothing left after the 9th iteration.
+sizes=(1 2 3 4 5 6 7 8 9)
 
 #FOLDER REFERENCES
 # Data="/Users/raouelletteiv/Research/7T_Spinal_Brain_Study/Data/Spinal_data/"
 Data=""  # run the script within the Data folder for it to work without specifying a custom Data variable
 
-# Run erosion on the PAM50 template
-mkdir erosion
-cd erosion
+# Download PAM50 template from SCT v3.0.1
+wget -O PAM50.zip https://osf.io/gdwn6/download
+unzip PAM50.zip
+cd PAM50/template/
 # Initialize PAM50_cord_rings.nii.gz file from cord segmentation
-cp $SCT_DIR/data/PAM50/template/PAM50_cord.nii.gz PAM50_cord_rings.nii.gz
-# Erode PAM50 cord mask several times, until there is nothing left
-for size in 1 2 3 4 5 6 7 8; do
-  sct_maths -i $SCT_DIR/data/PAM50/template/PAM50_cord.nii.gz -erode $size -shape disk -dim 2 -o PAM50_cord_e${size}.nii.gz
+cp PAM50_cord.nii.gz PAM50_cord_rings.nii.gz
+# Erode cord mask several times, until there is nothing left
+for size in ${sizes[@]}; do
+  # 2D erosion in the axial plane
+  sct_maths -i PAM50_cord.nii.gz -erode $size -shape disk -dim 2 -o tmp.PAM50_cord_rings_e${size}.nii.gz
 	# Iteratively add mask in order to create rings: each ring will have a specific label value: 1 (most outer ring), 2, 3, 4, etc.
-	sct_maths -i PAM50_cord_rings.nii.gz -add PAM50_cord_e${size}.nii.gz -o PAM50_cord_rings.nii.gz
+	sct_maths -i PAM50_cord_rings.nii.gz -add tmp.PAM50_cord_rings_e${size}.nii.gz -o PAM50_cord_rings.nii.gz
 done
+# remove temp stuff
+rm tmp.*
+# Create convenient variable pointing to file
+cord_rings=`pwd`/PAM50_cord_rings.nii.gz
 
-# Iterate across subjects and quantify in rings
-for ID in MS088_R01_20_sup; do
-#MS073_R01_05_inf MS073_R01_05_sup MS079_R01_11_inf MS079_R01_11_sup MS081_R01_13_inf MS081_R01_13_sup MS084_R01_16_sup MS088_R01_20_inf MS088_R01_20_mid MS088_R01_20_sup MS089_R01_29_inf MS089_R01_29_sup MS091_R01_22_inf MS091_R01_22_sup MS093_R01_24_sup MS095_R01_26_inf MS095_R01_26_sup MS096_R01_27_inf MS096_R01_27_sup MS100_R01_32_inf MS100_R01_32_sup MS101_R01_33_inf MS101_R01_33_sup MS102_R01_34_inf MS102_R01_34_sup MS103_R01_35_inf MS103_R01_35_sup MS104_R01_36_inf MS104_R01_36_sup NIMMS01_inf NIMMS01_mid NIMMS01_sup NIMMS02_inf NIMMS02_sup NIMMS03_sup NIMMS04_inf NIMMS04_sup NIMMS05_inf NIMMS05_sup NIMMS06_sup NIMMS07_inf NIMMS07_sup NIMMS08_inf NIMMS08_sup NIMMS09_inf NIMMS09_sup NIMMS10_inf NIMMS10_sup NIMMS11_inf NIMMS11_sup; do
-#for PATIENT_ID in ; do
+# Go back to local folder
+cd ../..
 
+# Iterate across subjects and quantify in rings, WM and GM
+subjects=(MS088_R01_20)
+for subject in ${subjects[@]}; do
 	# go to subject folder
-  cd ${Data}/${ID}/;
-
+  cd $subject
+  # Binarize lesion mask
+  fslmaths lesion_merged.nii.gz -thr 0.5 -bin lesion_merged_bin.nii.gz
+  # Multiply by rings
+  fslmaths lesion_merged_bin.nii.gz -mul $cord_rings lesion_merged_bin_rings.nii.gz
+  # Quantify lesion volume fraction in each sub-ring
+  for size in ${sizes[@]}; do
+    fslstats lesion_merged_bin_rings.nii.gz -l  $(($size-1)) -u  $(($size+1)) -V >> Whole.Lesion.Vol.txt
+  done
+done
 
 # OLD CODE
 # ========
